9694e038bd342f6de7affd8a140ad02ec408f8e2
Joseph Zucco <josephzucco@Josephs-MacBook-Pro.local>
2021-06-13 15:09:18 -0400

    recipe card

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function ProductCard({ product }) {
-    return (
-        <div>
-            {product.fields.title}
+  const { title, slug, cookingTime, thumbnail } = product.fields;
+
+  return (
+    <div className="card">
+      <div className="featured">{/* image - thumb */}</div>
+      <div className="content">
+        <div className="info">
+          <h4>{title}</h4>
+          <p>Takes approx {cookingTime} mins to make</p>
         </div>
-    )
+      </div>
+      <div className="actions">
+        <Link href={"/recipes/" + slug}>
+          <a>Cook this</a>
+        </Link>
+      </div>
+    </div>
+  );
 }
