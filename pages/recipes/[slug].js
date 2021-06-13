9b6c516bfb313de3b8e5d334b03613d8d4f2bfe4
Joseph Zucco <josephzucco@Josephs-MacBook-Pro.local>
2021-06-13 16:54:06 -0400

    install rich text package

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,6 @@
 import { createClient } from "contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -6,24 +8,63 @@ const client = createClient({
 });
 
 export const getStaticPaths = async () => {
-  const res = await.client.getEntries({
-    content_type: 'product'
-  })
+  const res = await client.getEntries({
+    content_type: "product",
+  });
 
-  const paths = res.items.map(item => {
+  const paths = res.items.map((item) => {
     return {
-      params: { slug: item.fields.slug }
-    }
-  })
+      params: { slug: item.fields.slug },
+    };
+  });
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "product",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: { product: items[0] },
+  };
 }
 
+export default function RecipeDetails({ product }) {
+  const { featuredImage, title, cookingTime, ingredients, method } =
+    product.fields;
 
+  return (
+    <div>
+      <div className="banner">
+        <Image
+          src={"https://" + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{title}</h2>
+      </div>
 
-export default function RecipeDetails() {
-  return <div>Recipe Details</div>;
+      <div className="info">
+        <p>${cookingTime}</p>
+        <h3>Ingredients:</h3>
+
+        {ingredients.map((ing) => (
+          <span key={ing}>{ing}</span>
+        ))}
+      </div>
+
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+
+      <style jsx></style>
+    </div>
+  );
 }
